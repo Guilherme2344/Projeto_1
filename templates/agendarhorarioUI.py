5bfeaa48958412225a37d48ed98ee089b8415238
Guilherme2344 <guilhermecsilva2.alternative@gmail.com>
2023-11-17 19:10:52 -0300

    Agenda

--- templates/agendarhorarioUI.py
+++ templates/agendarhorarioUI.py
@@ -13,23 +13,18 @@ class AgendaHorarioUI:
   def agendar_horario():
     lista_1 = []
     lista_2 = []
-    lista_3 = []
     for obj in View.agendar_listar_semana():
       if obj.get_id_cliente() == st.session_state['cliente_nome']:
-        lista_1.append([datetime.strftime(obj.get_data(), '%d/%m/%Y %H:%M'), obj.get_id_servico()])
-        lista_2.append(datetime.strftime(obj.get_data(), '%d/%m/%Y %H:%M'))
-        lista_3.append(obj.get_id_servico())
+        if obj not in View.nao_confirmado():
+          lista_1.append([datetime.strftime(obj.get_data(), '%d/%m/%Y %H:%M'), obj.get_id_servico()])
+          lista_2.append(obj)
     if len(lista_1) == 0: st.write("Nenhum horário disponível")
     else:
       df = pd.DataFrame(lista_1, columns=['Data', 'Serviço'])
       st.dataframe(df, hide_index=True)
-      horario = st.selectbox("Selecione um horário", lista_2)
-      servicos = st.selectbox("Selecione um serviço", set(lista_3))
-      for i in View.agenda_listar():
-        i = i.get_id() 
-      nome = st.session_state['cliente_nome']
+      op = st.selectbox('Selecione um serviço e seu horário', lista_2)
       if st.button("Agendar"):
-        View.agendar_horario(i, horario, False, nome, servicos)
+        View.agendar_horario(op.get_id(), datetime.strftime(op.get_data(), '%d/%m/%Y %H:%M'), False, op.get_id_cliente(), op.get_id_servico())
         st.success("Horário agendado com sucesso")
         time.sleep(2)
         st.rerun()
--- templates/confirmaragendamentoUI.py
+++ templates/confirmaragendamentoUI.py
@@ -14,11 +14,13 @@ class ConfirmarAgendamentosUI:
     for i in View.nao_confirmado():
         lista.append([i.get_id_cliente(), i.get_data(), i.get_id_servico(), i.get_confirmado()])
         lista_2.append(i)
-    df = pd.DataFrame(lista, columns=['Nome', 'Data', 'Serviço', 'Confirmado'])
-    st.dataframe(df, hide_index=True)
-    op = st.selectbox('Confirmar Agendamento', lista_2)
-    if st.button('Salvar'):
-       View.confirmar_agendamento(op.get_id(), op.get_data(), True, op.get_id_cliente(), op.get_id_servico())
-       st.success('Agendamento confirmado com sucesso')
-       time.sleep(2)
-       st.rerun()
+    if len(lista) == 0: st.write('Nenhum agendamento solicitado')
+    else:
+      df = pd.DataFrame(lista, columns=['Nome', 'Data', 'Serviço', 'Confirmado'])
+      st.dataframe(df, hide_index=True)
+      op = st.selectbox('Confirmar Agendamento', lista_2)
+      if st.button('Salvar'):
+        View.confirmar_agendamento(op.get_id(), op.get_data(), True, op.get_id_cliente(), op.get_id_servico())
+        st.success('Agendamento confirmado com sucesso')
+        time.sleep(2)
+        st.rerun()
